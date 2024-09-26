9f80052dcaee4113a2c378ac503d212dc45fd0cd
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-09-26 13:48:33 +0100

    enhancement: signupFetch working

---
+++ src/utils/fetch.js
@@ -0,0 +1,24 @@
+export const signupFetch = async (username, email, password) =>{
+    try {
+        const response = await fetch("http://localhost:5001/users/signup",{
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                email: email,
+                password: password,
+            }),
+        });
+    
+        const data = await response.json();
+        console.log("data in signupFetch: ", data);
+
+        return data;
+    } catch (error) {
+       console.log(error);
+       alert("there is an error. please check console"); 
+    }
+}
